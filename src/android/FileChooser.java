6a2add07d44c3803a904549b882af4155c9a58a4
Sito206 <66909083+Sito206@users.noreply.github.com>
2024-05-23 14:49:54 +0200

    Add files via upload
    Modificación para selección de multiples archivos devolviendo un array

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import android.content.ClipData;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -34,53 +35,59 @@ public class FileChooser extends CordovaPlugin {
         return false;
     }
 
-    public void chooseFile(JSONObject filter, CallbackContext callbackContext) {
-        String uri_filter = filter.has(MIME) ? filter.optString(MIME) : "*/*";
+public void chooseFile(JSONObject filter, CallbackContext callbackContext) {
+    String uri_filter = filter.has(MIME) ? filter.optString(MIME) : "*/*";
 
-        // type and title should be configurable
+    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+    intent.setType(uri_filter);
+    intent.addCategory(Intent.CATEGORY_OPENABLE);
+    intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true); // Permitir selección múltiple
 
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType(uri_filter);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+    Intent chooser = Intent.createChooser(intent, "Select File");
+    cordova.startActivityForResult(this, chooser, PICK_FILE_REQUEST);
 
-        Intent chooser = Intent.createChooser(intent, "Select File");
-        cordova.startActivityForResult(this, chooser, PICK_FILE_REQUEST);
+    PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+    pluginResult.setKeepCallback(true);
+    callback = callbackContext;
+    callbackContext.sendPluginResult(pluginResult);
+}
 
-        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-        pluginResult.setKeepCallback(true);
-        callback = callbackContext;
-        callbackContext.sendPluginResult(pluginResult);
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == PICK_FILE_REQUEST && callback != null) {
-
-            if (resultCode == Activity.RESULT_OK) {
-
-                Uri uri = data.getData();
-
-                if (uri != null) {
-
-                    Log.w(TAG, uri.toString());
-                    callback.success(uri.toString());
-
+  @Override
+public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == PICK_FILE_REQUEST && callback != null) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                ClipData clipData = data.getClipData();
+                if (clipData != null) {
+                    // Múltiples archivos seleccionados
+                    JSONArray uris = new JSONArray();
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        Uri uri = clipData.getItemAt(i).getUri();
+                        uris.put(uri.toString());
+                    }
+                    Log.w(TAG, uris.toString());
+                    callback.success(uris);
                 } else {
-
-                    callback.error("File uri was null");
-
+                    // Un solo archivo seleccionado
+                    Uri uri = data.getData();
+                    if (uri != null) {
+                        Log.w(TAG, uri.toString());
+                        callback.success(uri.toString());
+                    } else {
+                        callback.error("File uri was null");
+                    }
                 }
-
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                // keep this string the same as in iOS document picker plugin
-                // https://github.com/iampossible/Cordova-DocPicker
-                callback.error("User canceled.");
             } else {
-
-                callback.error(resultCode);
+                callback.error("Data was null");
             }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            callback.error("User canceled.");
+        } else {
+            callback.error(resultCode);
         }
     }
+}
+
 }
